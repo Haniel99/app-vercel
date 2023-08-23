8cab7b656181bb05abee8c929227c46d42fa6d04
AnibalN25 <aniballaura25@gmail.com>
2023-08-23 13:15:42 -0400

    first commissssstss

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-app.get("/", (req, res) => {
-    res.send("Hello word");
-})
+app.get('*',(req,res,next)=>{
+    res.status(200).json({
+      message:'bad request'
+    })
+  })
 app.listen(port, ()=>{
     console.log("the app is nunning on: ", port);
 });
