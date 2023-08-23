327453e68ecb161c7e4a39628e07f2054a9059f0
AnibalN25 <aniballaura25@gmail.com>
2023-08-23 12:45:30 -0400

    first commisssst

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+app.get("/", (req, res) => {
+    res.send("Hello word");
+})
 app.listen(port, ()=>{
     console.log("the app is nunning on: ", port);
 });
